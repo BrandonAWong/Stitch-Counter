34bcd2b4d5accaee6395f923f2f2055829a84da2
Brandon Wong <bwong@rsihomeproducts.com>
2024-06-28 14:04:42 -0700

    finished

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+function getIntRange(msg, min, max) {
+    const count = prompt(msg);
+    if (count && isFinite(count) && min <= count && max >= count) {
+        return count;
+    }
+    else if (count) {
+        alert('Invalid input. Must be number between 0-99.');
+    }
+}
+
+function setInfo(sets, repsPerSet) {
+    if (sets <= 0) {
+        return;
+    }
+    setStitchesLeftForSet(repsPerSet);
+    setCurrentSet(repsPerSet);
+    setFinishedSets(repsPerSet);
+    setSetsLeft(sets, repsPerSet);
+}
+
+function setStitchesLeftForSet(repsPerSet) {
+    const stitches = document.querySelector('.stitchCounter').textContent;
+    document.querySelector('.repsLeft').textContent = `Reps Left For Set: ${repsPerSet - (stitches % repsPerSet)}`;
+}
+
+function setCurrentSet(repsPerSet) {
+    const stitches = document.querySelector('.stitchCounter').textContent;
+    document.querySelector('.currentSet').textContent = `Working On Set: ${Math.floor(stitches / repsPerSet) + 1}`;
+}
+
+function setFinishedSets(repsPerSet) {
+    const stitches = document.querySelector('.stitchCounter').textContent;
+    document.querySelector('.setsFinished').textContent = `Sets Finished: ${Math.floor(stitches / repsPerSet)}`;
+}
+
+function setSetsLeft(sets, repsPerSet) {
+    const stitches = document.querySelector('.stitchCounter').textContent;
+    document.querySelector('.setsLeft').textContent = `Sets Left: ${sets - Math.floor(stitches / repsPerSet)}`;
+}
+
 function add() {
     const counterBox = document.querySelector('.stitchCounter');
     if (counterBox.textContent >= 99) {
@@ -14,52 +54,56 @@ function minus() {
     --counterBox.textContent;
 }
 
-function getIntRange(msg, min, max) {
-    const count = prompt(msg);
-    if (count && isFinite(count) && min <= count && max >= count) {
-        return count;
-    }
-    else if (count) {
-        alert('Invalid input. Must be number between 0-99.');
-    }
-}
-
 window.onload = () => {
-    const sets = 0;
-    const reps = 0;
+    let sets = 0;
+    let reps = 1;
 
     document.querySelector('body').addEventListener('keyup', (e) => {
         if (e.key === 'F5') {
             return;
         }
-        add();
+        if (e.key === ' ') {
+            add();
+            setInfo(sets, reps);
+        }
     });
+
     document.querySelector('.add').addEventListener('click', (e) => {
         add();
-        e.target.blur()
+        e.target.blur();
+        setInfo(sets, reps);
     });
+
     document.querySelector('.minus').addEventListener('click', (e) => {
         minus();
         e.target.blur()
+        setInfo(sets, reps);
     });
 
     document.querySelector('.setSets').addEventListener('click', () => {
         const setCount = getIntRange('Enter how many sets for this round', 0, 99);
-        if (!setCount) { return; }
+        if (!setCount) { 
+            return; 
+        }
         const repCount = getIntRange('Enter how many reps per set', 0, 99);
-        if (!repCount) { return; }
+        if (!repCount) { 
+            return; 
+        }
 
         sets = setCount;
         reps = repCount;
-
-        document.querySelector('.totalSets').textContent = setCount;
+        document.querySelector('.totalSets').textContent = `Total Sets: ${sets}`;
+        setInfo(sets, reps);
     });
+
     document.querySelector('.setCount').addEventListener('click', () => {
         const count = getIntRange('Enter stitch count', 0, 99);
         if (count) {
             document.querySelector('.stitchCounter').textContent = count;
+            setInfo(sets, reps);
         }
     });
+
     document.querySelector('.reset').addEventListener('click', () => {
         if (window.confirm('Reset stitch count back to 0?')) {
             document.querySelector('.stitchCounter').textContent = 0;
